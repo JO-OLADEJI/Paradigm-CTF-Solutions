e80d389aa1f8e408ea820880b4783456ab406f12
Joshua Oladeji <dejijolaoluwa@gmail.com>
2025-07-20 05:57:07 +0100

    add breakdown/contract annotations to Swap challenge

--- src/2021/05.Swap.sol
+++ src/2021/05.Swap.sol
@@ -4,31 +4,47 @@ import "./05.ERC20.sol";
 import "./05.ReentrancyGuard.sol";
 
 contract StableSwap is ReentrancyGuard {
-    address private owner;
-
-    ERC20Like[] public underlying;
-    mapping(address => bool) public hasUnderlying;
-
-    uint private supply;
-    mapping(address => uint) private balances;
-    mapping(address => mapping(address => uint)) private approvals;
+    // @audit-info                                                          STORAGE LAYOUT
+    // ReentrancyGuard._status (_NOT_ENTERED = 1, _ENTERED = 2)                       0x00
+    address private owner; //                                                         0x01
+    ERC20Like[] public underlying; //                                                 0x02
+    mapping(address => bool) public hasUnderlying; //                                 0x03
+    uint private supply; //                                                           0x04
+    mapping(address => uint) private balances; //                                     0x05
+    mapping(address => mapping(address => uint)) private approvals; //                0x06
 
     constructor() public {
         owner = msg.sender;
     }
 
     struct MintVars {
+        // amount of LP tokens in circulation
         uint totalSupply;
+        // ---
+        // normalized (to `18` decimals) this contract's balance of underlying tokens before receiving liquidity
         uint totalBalanceNorm;
+        // ---
+        // normalized (to `18` decimals) the total underlying tokens received by this contract
         uint totalInNorm;
         uint amountToMint;
+        // ---
+        // address of ERC20 (stablecoin) currently being used to provide liquidity
         ERC20Like token;
+        // ---
+        // `token` balance of `msg.sender`
         uint has;
+        // ---
+        // `token` balance of this contract before receiving liquidty
         uint preBalance;
+        // ---
+        // `token` balance of this contract after receiving liquidty
         uint postBalance;
+        // ---
+        // net difference between `postBalance` and `preBalance`, reflecting the exact amount this contract received
         uint deposited;
     }
 
+    // @audit-info: send fresh LP tokens to an address in proportion to the liquidity specified in `amounts`
     function mint(uint[] memory amounts) public nonReentrant returns (uint) {
         MintVars memory v;
         v.totalSupply = supply;
@@ -39,8 +55,13 @@ contract StableSwap is ReentrancyGuard {
             v.preBalance = v.token.balanceOf(address(this));
 
             v.has = v.token.balanceOf(msg.sender);
+
+            // @audit-info: if `amount` of underlying token is more than the `msg.sender` balance of that token,
+            // then replace the `amount` with the sender's balance.
+            // basically `amounts[i]` <= token.balanceOf(msg.sender)
             if (amounts[i] > v.has) amounts[i] = v.has;
 
+            // @audit-info: transfer the `amount` from the sender to this contract
             v.token.transferFrom(msg.sender, address(this), amounts[i]);
 
             v.postBalance = v.token.balanceOf(address(this));
@@ -48,12 +69,16 @@ contract StableSwap is ReentrancyGuard {
             v.deposited = v.postBalance - v.preBalance;
 
             v.totalBalanceNorm += scaleFrom(v.token, v.preBalance);
+
             v.totalInNorm += scaleFrom(v.token, v.deposited);
         }
 
+        // first liquidity provider gets LP tokens equal to net liquidity received by this contract
         if (v.totalSupply == 0) {
             v.amountToMint = v.totalInNorm;
-        } else {
+        }
+        // other liquidity providers get LP tokens proportional to the balance of liquidity already held by this contract
+        else {
             v.amountToMint =
                 (v.totalInNorm * v.totalSupply) /
                 v.totalBalanceNorm;
@@ -66,11 +91,17 @@ contract StableSwap is ReentrancyGuard {
     }
 
     struct BurnVars {
+        // amount of LP tokens in circulation
         uint supply;
+        // ---
+        // a single underlying `token` balance of this contract before sending liquidty
         uint haveBalance;
+        // ---
+        // single underlying `token` amount to send to a liquidity provider
         uint sendBalance;
     }
 
+    // @audit-info: converts LP tokens to underlying tokens, proportional to their balances held in this contract
     function burn(uint amount) public nonReentrant {
         require(balances[msg.sender] >= amount, "burn/low-balance");
 
@@ -79,6 +110,8 @@ contract StableSwap is ReentrancyGuard {
 
         for (uint i = 0; i < underlying.length; i++) {
             v.haveBalance = underlying[i].balanceOf(address(this));
+
+            // @audit-notiice: proportional withdrawal of liquidity from `underlying` tokens
             v.sendBalance = (v.haveBalance * amount) / v.supply;
 
             underlying[i].transfer(msg.sender, v.sendBalance);
@@ -89,10 +122,21 @@ contract StableSwap is ReentrancyGuard {
     }
 
     struct SwapVars {
+        // this contract's balance of `src` ERC20 token before pulling funds from sender
+        // also this contract's balance of `dst` ERC20 token before sending funds to user
         uint preBalance;
+        // ---
+        // this contract's balance of `src` ERC20 token after pulling funds from sender
+        // also this contract's balance of `dst` ERC20 token after sending funds to user
         uint postBalance;
+        // ---
+        // net-inflow of `src` token - after misc fees (with 0.3% fee applied)
         uint input;
+        // ---
+        // calculated amount of `dst` tokens to be sent to caller based on `input`
         uint output;
+        // ---
+        // net-outflow of `dst` token - after misc fees
         uint sent;
     }
 
@@ -105,12 +149,15 @@ contract StableSwap is ReentrancyGuard {
         require(hasUnderlying[address(dst)], "swap/invalid-dst");
 
         SwapVars memory v;
+
         v.preBalance = src.balanceOf(address(this));
         src.transferFrom(msg.sender, address(this), srcAmt);
         v.postBalance = src.balanceOf(address(this));
 
+        // @audit-info: net-inflow (minus 0.3% fee)
         v.input = ((v.postBalance - v.preBalance) * 997) / 1000;
 
+        // @audit-info: converts normalized amountIn to native decimals of `dst` ERC20 token
         v.output = scaleTo(dst, scaleFrom(src, v.input));
 
         v.preBalance = dst.balanceOf(address(this));
@@ -122,37 +169,57 @@ contract StableSwap is ReentrancyGuard {
         require(v.sent <= v.output, "swap/bad-token");
     }
 
+    // @audit-info: normalizes the `value` of the given `tokenn` to 18 decimals
     function scaleFrom(ERC20Like token, uint value) internal returns (uint) {
         uint decimals = token.decimals();
+
+        // for instance, DAI with `18` decimals, 10¹⁸
         if (decimals == 18) {
             return value;
-        } else if (decimals < 18) {
+        }
+        // for instance, USDC with `6` decimals
+        // 10⁶ * 10⁽¹⁸⁻⁻⁶⁾
+        else if (decimals < 18) {
             return value * 10 ** (18 - decimals);
-        } else {
+        }
+        // for instance xTOKEN with `24` decimals
+        // (10²⁴ * 10¹⁸) / 10²⁴
+        else {
             return (value * 10 ** 18) / 10 ** decimals;
         }
     }
 
+    /// @audit-info: converts a normalized `value` (18 decimals) of a praticular `token` to it's native decimal
     function scaleTo(ERC20Like token, uint value) internal returns (uint) {
         uint decimals = token.decimals();
+
+        // for instance, DAI with `18` decimals, 10¹⁸
         if (decimals == 18) {
             return value;
-        } else if (decimals < 18) {
+        }
+        // for instance, USDC with `6` decimals
+        // (10¹⁸ * 10⁶) / 10¹⁸
+        else if (decimals < 18) {
             return (value * 10 ** decimals) / 10 ** 18;
-        } else {
+        }
+        // for instance xTOKEN with `24` decimals
+        // 10¹⁸ * 10⁽²⁴⁻⁻¹⁸⁾
+        else {
             return value * 10 ** (decimals - 18);
         }
     }
 
+    // @audit-info: allows owner of LP tokens transfer them to an arbitrary address
     function transfer(address to, uint amount) public returns (bool) {
         require(balances[msg.sender] >= amount, "transfer/low-balance");
 
         balances[msg.sender] -= amount;
-        balances[to] += amount;
+        balances[to] += amount; // @audit-notice: theoretically, this can overflow
 
         return true;
     }
 
+    // @audit-info: allows transferring LP tokens on-approval of `from` address
     function transferFrom(
         address from,
         address to,
@@ -171,12 +238,15 @@ contract StableSwap is ReentrancyGuard {
         return true;
     }
 
+    // @audit-info: allows an amount of sender's funds to be transfered by arbitrary address
+    // @audit-notice: can be front-ran to spend allowance before allowing a new amount
     function approve(address who, uint amount) public returns (bool) {
         approvals[msg.sender][who] = amount;
 
         return true;
     }
 
+    // @audit-info: returns the amount of `who` tokens the `spender` is allowed to transfer
     function allowance(
         address who,
         address spender
@@ -184,40 +254,53 @@ contract StableSwap is ReentrancyGuard {
         return approvals[who][spender];
     }
 
+    // @audit-info: returns the LP tokens balance of an address
     function balanceOf(address who) public returns (uint) {
         return balances[who];
     }
 
+    // @audit-info: returns amount of LP tokens minted (represents collateral in this token)
     function totalSupply() public view returns (uint) {
         return supply;
     }
 
+    // @audit-info: basic decimals of ERC20 LP tokens
     function decimals() public view returns (uint8) {
         return 18;
     }
 
+    // @audit-info: basic name of ERC20 LP tokens
     function name() public view returns (string memory) {
         return "StableSwap v1.0";
     }
 
+    // @audit-info: basic symbol of ERC20 LP tokens
     function symbol() public view returns (string memory) {
         return "USDSWAP";
     }
 
+    // @audit-info: returns the total balance of underlying tokens in this contract - normalized to `18` decimals
     function totalValue() public view returns (uint) {
         uint value = 0;
         for (uint i = 0; i < underlying.length; i++) {
             value += scaleFrom(
-                underlying[i],
-                underlying[i].balanceOf(address(this))
+                underlying[i], // `token` - DAI | TrueUSD | USDC | USDT
+                underlying[i].balanceOf(address(this)) // balanceOf `token` this address holds
             );
         }
         return value;
     }
 
+    // @audit-info: function for owner to add collateral tokens accepted by the pool
     function addCollateral(ERC20Like collateral) public {
         require(msg.sender == owner, "addCollateral/not-owner");
 
+        // @audit-question: not the best setup to have 2 sources of truth... out of sync possible?
+        // From the `Setup` contract, underlying tokens are:
+        // 1. DAI
+        // 2. TrueUSD
+        // 3. USDC
+        // 4. USDT
         underlying.push(collateral);
         hasUnderlying[address(collateral)] = true;
     }
--- test/2021/05.Swap.t.sol
+++ test/2021/05.Swap.t.sol
@@ -19,14 +19,17 @@ contract SetupSwap {
         swap = new StableSwap();
 
         UniswapV2RouterLike router = UniswapV2RouterLike(
-            0xf164fC0Ec4E93095b804a4795bBe1e041497b92a
+            0xf164fC0Ec4E93095b804a4795bBe1e041497b92a // UniswapV2 router - mainnet
         );
 
+        // @audit-notice: lookout for ERC20 tokens that don't revert on fail but return `false`
+        // @audit-notice: lookout for rebase ERC20 tokens
+        // @audit-notice: lookout for tokens that have multiple access points => TrueUSD
         ERC20Like[4] memory tokens = [
-            ERC20Like(0x6B175474E89094C44Da98b954EedeAC495271d0F),
-            ERC20Like(0x0000000000085d4780B73119b644AE5ecd22b376),
-            ERC20Like(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48),
-            ERC20Like(0xdAC17F958D2ee523a2206206994597C13D831ec7)
+            ERC20Like(0x6B175474E89094C44Da98b954EedeAC495271d0F), // DAI - mainnet | @audit-notice: flashloan mint on this token is possible
+            ERC20Like(0x0000000000085d4780B73119b644AE5ecd22b376), // TrueUSD - mainnet | @audit-notice: I know this token is weird
+            ERC20Like(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48), // USDC - mainnet | @audit-notice: I know this token is a bit centralized
+            ERC20Like(0xdAC17F958D2ee523a2206206994597C13D831ec7) // USDT - mainnet | @audit-notice: weird token e.g addresses can be blacklisted
         ];
 
         uint[] memory amounts = new uint[](4);
@@ -35,8 +38,8 @@ contract SetupSwap {
             swap.addCollateral(tokens[i]);
 
             address[] memory path = new address[](2);
-            path[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-            path[1] = address(tokens[i]);
+            path[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // WETH - mainnet
+            path[1] = address(tokens[i]); // Each stablecoin in the `tokens` array
 
             router.swapExactETHForTokens.value(100 ether)(
                 0,
@@ -45,8 +48,8 @@ contract SetupSwap {
                 uint(-1)
             );
 
-            tokens[i].approve(address(swap), uint(-1));
-            amounts[i] = tokens[i].balanceOf(address(this));
+            tokens[i].approve(address(swap), uint(-1)); // @audit-info: approve `type(uint256).max` for `StableSwap` contract
+            amounts[i] = tokens[i].balanceOf(address(this)); // @audit-info: store the received tokens amount in `amounts` array
         }
 
         swap.mint(amounts);
@@ -54,6 +57,7 @@ contract SetupSwap {
         value = swap.totalValue();
     }
 
+    // @audit-info: criteria for passing this challenge is to reduce the `totalValue` of `StableSwap` contract to less than 1/100th it's original value
     function isSolved() public view returns (bool) {
         return swap.totalValue() < value / 100;
     }
